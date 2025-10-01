5f9e3952db897598c57e5f206d5dbd1cd30bb7ac
sol4nki <solankipranjal44@gmail.com>
2025-10-01 16:57:48 +0530

    fixing design and other issues

--- script.js
+++ script.js
@@ -10,11 +10,11 @@
 
 // const clickedId = null
 
-function shrinklvl(clickedId){
-    if (clickedId){
-        clickedId.id = "content";
-    }
-}
+// function shrinklvl(clickedId){
+//     if (clickedId){
+//         clickedId.id = "content";
+//     }
+// }
 
 // function expandlvlClick(clickedId){
 //     if (clickedId){
@@ -44,3 +44,115 @@ lvlclass.forEach(element => {
 
     })
 })
+
+
+
+// home page stuff time, online, lvls left
+
+// for lvls-left use server callbacks
+
+const midnight = new Date()
+midnight.setHours(24,0,0,0)
+
+function timeleft(){
+    const now = new Date()
+    
+
+    const difference = midnight - now
+    
+    if (difference < 0){
+        midnight.setDate(midnight.getDate + 1)
+        const newNow = now.getTime()
+        const newDifference = midnight - newNow
+
+        
+        const fulltime = Math.floor(newDifference / 1000)
+        const hours = Math.floor(fulltime / 3600)
+        const minutes = Math.floor((fulltime % 3600) / 60)
+        const seconds = fulltime % 60
+
+        const formattedh = String(hours).padStart(2, '0');
+        const formattedm = String(minutes).padStart(2, '0');
+        const formatteds = String(seconds).padStart(2, '0');
+        
+        const timeid = document.getElementById('time')
+        if (timeid) {
+            timeid.textContent = `${formattedh}:${formattedm}:${formatteds}`
+        }
+
+        
+    }else{
+        const fulltime = Math.floor(difference / 1000)
+        const hours = Math.floor(fulltime / 3600)
+        const minutes = Math.floor((fulltime % 3600) / 60)
+        const seconds = fulltime % 60
+        // console.log(totalSeconds, hours, minutes, seconds)
+        const formattedh = String(hours).padStart(2, '0');
+        const formattedm = String(minutes).padStart(2, '0');
+        const formatteds = String(seconds).padStart(2, '0');
+
+
+
+        const timeid = document.getElementById('time');
+        
+        if (timeid) {
+            timeid.textContent = `${formattedh}:${formattedm}:${formatteds}`
+        }
+    }
+}
+
+function challleft(){
+    const challleft = "check server call int"
+    
+    // console.log(new Date())
+    
+    if (0 <= challleft <= 10){
+        const difference = 10 - challleft
+
+        const challid = document.getElementById('chall')
+        if (challid) {
+            challid.textContent = `${difference}`
+        }
+
+        
+    }else{
+        const challid = document.getElementById('chall')
+        if (challid) {
+            challid.textContent = `error`
+        }
+    }
+}
+
+function serverstatus(){
+    const ping = "check for ping on server callvback"
+    
+    
+    if (ping){
+
+        const serverid = document.getElementById('serverstats')
+        if (serverid) {
+            serverid.textContent = `$online`
+        }
+
+        
+    }else{
+        const serverid = document.getElementById('serverstats')
+        if (serverid) {
+            serverid.textContent = `$offline`
+        }
+    }
+}
+
+
+timeleft()
+challleft()
+serverstatus() //only need to update once when website loads
+
+setInterval(() => {
+    
+    timeleft()
+}, 1200);
+
+setInterval(() => {
+    challleft()
+}, 240000); //no need to update alot just 3-4 mins do it 
